1c0412ebb261135b4cebb0371b7b4132b6222589
Oakekywe <oakekywephyohann99@gmail.com>
2023-03-11 00:06:32 +0630

    delete user

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Http\Requests\UserUpdateRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -35,9 +38,16 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-        //
+        User::create([
+            'username' => $request->username,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return redirect()->route('users.index')->with('message', 'New User Registered!');
     }
 
     /**
@@ -69,9 +79,15 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UserUpdateRequest $request, User $user)
     {
-        //
+        $user->update([
+            'username' => $request->username,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+        ]);
+        return redirect()->route('users.index')->with('message', 'User successfully Updated!');
     }
 
     /**
@@ -80,8 +96,12 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        if (auth()->user()->username == $user->username) {
+            return redirect()->route('users.index')->with('error','You cannot delete yourself!');
+        }
+        $user->delete();
+        return redirect()->route('users.index')->with('message', 'User Successfully Deleted!'); 
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,6 +5,22 @@
     </div>
     <div class="row">
         <div class="card mx-auto">
+            {{-- alert box --}}
+            <div>
+                @if (session('message'))
+                    <div class="alert alert-success">
+                        {{session('message')}}
+                    </div>
+                @endif
+            </div>
+            <div>
+                @if (session('error'))
+                    <div class="alert alert-danger">
+                        {{session('error')}}
+                    </div>
+                @endif
+            </div>
+
             <div class="card-header">
                 <a href="{{route('users.create')}}" class="float-right">Create</a>
             </div>
@@ -24,7 +40,9 @@
                                 <th scope="row">{{$user->id}}</th>
                                 <td>{{$user->username}}</td>
                                 <td>{{$user->email}}</td>
-                                <td>Edit / Delete</td>
+                                <td>
+                                <a class="btn btn-success" href="{{route('users.edit', $user->id)}}">Edit</a>
+                                </td>
                             </tr>
                         @endforeach                        
                     </tbody>

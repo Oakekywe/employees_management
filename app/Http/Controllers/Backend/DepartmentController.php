3d8c09c31e745e1369ca594b5da87bec2e36b949
Oakekywe <oakekywephyohann99@gmail.com>
2023-03-12 17:33:13 +0630

    some fixed

--- app/Http/Controllers/Backend/DepartmentController.php
+++ app/Http/Controllers/Backend/DepartmentController.php
@@ -13,9 +13,12 @@ class DepartmentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $req)
     {
         $departments= Department::all();
+        if ($req->search) {
+            $departments = Department::where('name', 'like', "%{$req->search}%")->get();
+        }
         return view('departments.index', compact('departments'));
     }
 
